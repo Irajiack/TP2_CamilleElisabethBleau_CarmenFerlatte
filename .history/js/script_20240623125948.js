84beaac37e072c06efdd6a6b02e5ffbb11d60c06
Carmen Ferlatte <carmen.ferlatte@gmail.com>
2024-06-23 13:02:37 -0400

    ajout du script js pour le menu burger

---
+++ .history/js/script_20240623125948.js
@@ -0,0 +1,10 @@
+  // menu toggle
+
+document.addEventListener('DOMContentLoaded', function () {
+    const menuToggle = document.getElementById('menu-toggle');
+    const menu = document.getElementById('menu');
+
+    menuToggle.addEventListener('click', function () {
+      menu.classList.toggle('hidden');
+    });
+  });
